02822780720b5f17299164be0cb8201dfb8259ee
enya-yyjr <ak47ym123@sina.com>
2021-01-01 03:31:36 +0800

    코틸리티 FrogJmp 문제 해결

---
+++ otherProblem/FrogJmp.js
@@ -0,0 +1,8 @@
+// 문제 링크( 코딜리티 회원가입 필요) : https://app.codility.com/c/run/trainingY7NRDE-ZS2/
+
+function solution(X, Y, D) {
+  return Math.ceil((Y - X) / D);
+}
+
+console.log(solution(10, 85, 30));
+// console.log(solution());
